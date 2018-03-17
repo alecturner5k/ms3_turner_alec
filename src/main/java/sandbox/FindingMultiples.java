f99983dab08c99812d423a1bd385aa2ef460c095
Alec_Turner <alec@swamps.com>
2018-03-17 16:29:53 -0500

    first/final commit

---
+++ src/main/java/sandbox/FindingMultiples.java
@@ -0,0 +1,49 @@
+package sandbox;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class FindingMultiples {
+	
+	public List<String> multiplesInRange(int[] numberRange)
+	{
+		
+		List<String> result = new ArrayList<>();
+		
+		for(int i = 0; i < numberRange.length; i++)
+		{
+			if(numberRange[i] % 3 == 0 && numberRange[i] % 7 == 0)
+			{
+				result.add(numberRange[i] + " : " + "MS3 and ME");
+				i++;
+			}else if(numberRange[i] % 7 == 0)
+			{
+				result.add(numberRange[i] + " : " +  "MS3");
+				i++;
+			}else if(numberRange[i] % 3 == 0)
+			{
+				result.add(numberRange[i] + " : " + "ME");
+				i++;
+			}else
+			{
+				i++;
+			}
+		}
+		
+		return result;
+	}
+
+	
+	public int[] getRange(int floor, int ceiling)
+	{
+		int[] range;
+		
+		if(floor > ceiling)
+		{
+			 return range = IntStream.rangeClosed(ceiling, floor).toArray();
+		}else
+		{
+			 return range = IntStream.rangeClosed(floor, ceiling).toArray();
+		}
+	}
+}
